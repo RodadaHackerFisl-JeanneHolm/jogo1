8182161d0c502016871374f535b8f204fe9e2065
Fabricio C Zuardi <fabricio@fabricio.org>
2014-05-07 15:15:50 -0300

    adicionei um id na tag de imagem e no script coloquei uma funcao que é executada apenas depois que a pagina terminou de carregar

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-	
-	Math.floor((Math.random() * 20) + 1);
-	
+
+console.log(Math.floor((Math.random() * 20) + 1));
+
+
+window.addEventListener('load', inicio);
+
+function inicio(){
+    var elementoImagem = document.getElementById("imagem");
+
+
+    console.log(elementoImagem);
+
+}
