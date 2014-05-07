01d2f2b48dcb2052ca4957cb136d305fbe4544d2
Fabricio C Zuardi <fabricio@fabricio.org>
2014-05-07 16:52:01 -0300

    limpei umas linhas comentadas

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 
-//console.log(Math.floor((Math.random() * 20) + 1));
+
 
 function sorteiaNome(){
     var x= Math.floor(Math.random() * 20);
     return lista[x];
 }
- // console.log(x+ ".png");
-// function (Math.floor((Math.random() * 20) + 1));
 
 window.addEventListener('load', inicio);
 
@@ -21,7 +19,6 @@ function inicio(){
 var lista = [];
 for (i= 1; i<= 20; i++) {
     var nome = "imagens/" +i+ ".png";
-	// console.log(nome);
     lista.push(nome);
 }
 console.log(lista);
